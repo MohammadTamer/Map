ab9197664d1fec3edf4741b43bc0d5c88dd25853
Mohammad Alshafie <114305911+MohammadTamer@users.noreply.github.com>
2025-05-08 22:41:23 +0300

    input map details

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,93 +1,119 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Globalization;
-using System.Diagnostics;
+using System.Linq;
 
-namespace MapRouting
+public class MapDetails
 {
-    // 1) Basic types
-    public class Node
+    public int IntersectionCount { get; set; }
+    public int RoadCount { get; set; }
+    public List<Intersection> Intersections { get; set; } = new List<Intersection>();
+    public List<Road> Roads { get; set; } = new List<Road>();
+}
+
+public class Intersection
+{
+    public int Id { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+}
+
+public class Road
+{
+    public int FirstPoint { get; set; }
+    public int SecondPoint { get; set; }
+    public double Length { get; set; }
+    public double Speed { get; set; }
+}
+
+public class DataReader
+{
+    public static MapDetails ReadMap(string path)
     {
-        public int Id;
-        public double X, Y;
-        public List<Edge> Neighbors = new List<Edge>();
+        // Create a new MapDetails instance to store our map data.
+
+        MapDetails mapData = new MapDetails();
+
+        if (File.Exists(path))
+        {
+            // Read all lines from the file.
+            string[] lines = File.ReadAllLines(path);
+            int lineNumber = 0;
+            // First line: number of intersections.
+            if (lineNumber < lines.Length)
+            {
+                mapData.IntersectionCount = int.Parse(lines[lineNumber++]);
+            }
+            // Next lines: details for each intersection.
+            for (int i = 0; i < mapData.IntersectionCount; i++)
+            {
+                if (lineNumber < lines.Length)
+                {
+                    string[] parts = lines[lineNumber++].Split(' ');
+                    if (parts.Length == 3)
+                    {
+                        int id = int.Parse(parts[0]);
+                        double x = double.Parse(parts[1]);
+                        double y = double.Parse(parts[2]);
+                        Intersection tmp = new Intersection();
+                        tmp.Id = id;
+                        tmp.X = x;
+                        tmp.Y = y;
+                        mapData.Intersections.Add(tmp);
+                    }
+                }
+            }
+            // Next line: number of roads.
+            if (lineNumber < lines.Length)
+            {
+                mapData.RoadCount = int.Parse(lines[lineNumber++]);
+            }
+            // Following lines: details for each road.
+            for (int i = 0; i < mapData.RoadCount; i++)
+            {
+                if (lineNumber < lines.Length)
+                {
+                    string[] parts = lines[lineNumber++].Split(' ');
+                    if (parts.Length == 4)
+                    {
+                        int firstId = int.Parse(parts[0]);
+                        int secondId = int.Parse(parts[1]);
+                        double length = double.Parse(parts[2]);
+                        double speed = double.Parse(parts[3]);
+                        Road tmp = new Road();
+                        tmp.FirstPoint = firstId;
+                        tmp.SecondPoint = secondId;
+                        tmp.Length = length;
+                        tmp.Speed = speed;
+                        mapData.Roads.Add(tmp);
+                    }
+                }
+            }
+        }
+        return mapData;
     }
 
-    public class Edge
+    public static void Main(string[] args)
     {
-        public Node Target;
-        public double LengthKm;    // in km
-        public double SpeedKph;    // in km/h
-        public double TimeHours => LengthKm / SpeedKph;
-    }
+        // Assign the Map here
+        string path = "map1.txt";
 
-    // 2) Graph loader
-    public class Graph
-    {
-        public Dictionary<int, Node> Nodes = new Dictionary<int, Node>();
+        MapDetails mapData = ReadMap(path);
 
-        public static Graph LoadFromFile(string mapPath)
+        // Displaying The Map
+        if (mapData != null)
         {
-            var graph = new Graph();
-            var lines = File.ReadAllLines(mapPath);
-            int idx = 0;
-            int n = int.Parse(lines[idx++]);
-            // read nodes
-            for (int i = 0; i < n; i++)
+            Console.WriteLine(mapData.IntersectionCount);
+            foreach (var i in mapData.Intersections)
             {
-                var parts = lines[idx++].Split();
-                int id = int.Parse(parts[0]);
-                double x = double.Parse(parts[1], CultureInfo.InvariantCulture);
-                double y = double.Parse(parts[2], CultureInfo.InvariantCulture);
-                graph.Nodes[id] = new Node { Id = id, X = x, Y = y };
+                Console.WriteLine($"{i.Id} {i.X} {i.Y}");
             }
-            int m = int.Parse(lines[idx++]);
-            // read edges
-            for (int i = 0; i < m; i++)
+            Console.WriteLine(mapData.RoadCount);
+            foreach (var i in mapData.Roads)
             {
-                var p = lines[idx++].Split();
-                int u = int.Parse(p[0]), v = int.Parse(p[1]);
-                double len = double.Parse(p[2], CultureInfo.InvariantCulture);
-                double spd = double.Parse(p[3], CultureInfo.InvariantCulture);
-                var nu = graph.Nodes[u];
-                var nv = graph.Nodes[v];
-                nu.Neighbors.Add(new Edge { Target = nv, LengthKm = len, SpeedKph = spd });
-                nv.Neighbors.Add(new Edge { Target = nu, LengthKm = len, SpeedKph = spd });
+                Console.WriteLine($"{i.FirstPoint} {i.SecondPoint} {i.Length} {i.Speed}");
             }
-            return graph;
         }
-    }
-
-    // 3) Query processing & main
-    class Program
-    {
-        // walking speed constant
-        const double WalkKph = 5.0;
-
-        static void Main(string[] args)
-        {
-            if (args.Length < 2)
-            {
-                Console.WriteLine("Usage: MapRouting.exe <mapFile> <queryFile>");
-                return;
-            }
-            var graph = Graph.LoadFromFile(args[0]);
-
-            // TODO: load queries, for each:
-            // 1. Find startCandidates = all nodes with euclidDist(src, node) <= R/1000
-            // 2. Find endCandidates similarly
-            // 3. Build a priority-queue Dijkstra over a virtual graph:
-            //    – from src, can “walk” to each start node at time = dist/5
-            //    – all road-edges as defined
-            //    – from each end node, “walk” to dest.
-            // 4. Reconstruct path & compute metrics
-        }
-
-        static double EuclidDistKm(double x1, double y1, double x2, double y2)
-        {
-            double dx = x1 - x2, dy = y1 - y2;
-            return Math.Sqrt(dx * dx + dy * dy);
-        }
+        Console.ReadLine();
     }
 }
